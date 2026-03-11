ecafa75ac00f947fbbcfad89615c6b117a31efef
liyitian <yitian.25@intl.zju.edu.cn>
2026-03-11 15:18:02 +0800

    fix the place that not all the wrong input can be given out

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -6,14 +6,15 @@ age = float(input("Please enter the age: "))
 weight = float(input("Please enter the weight: "))
 cr = float(input("Please enter the creatine concentration: "))
 gender = input("Please enter the gender (male/female): ").lower()#get information about the age of the patient, the weight, the creatine concentration, and the gender of the patient.
-if age >= 100 :
-    print("Age is not correct")
-elif weight < 20 or weight > 80:
-    print("Weight is not correct")
-elif cr <= 0 or cr >= 100:
-    print("Creatine concentration is not correct")
-elif gender not in ["male", "female"]:
-    print("Gender is not correct")#make sure that the input values are within the specified ranges
+if age >= 100 or weight < 20 or weight > 80 or cr <= 0 or cr >= 100 or gender not in ["male","female"]:
+    if age >= 100 :
+        print("Age is not correct")
+    if weight < 20 or weight > 80:
+        print("Weight is not correct")
+    if cr <= 0 or cr >= 100:
+        print("Creatine concentration is not correct")
+    if gender not in ["male", "female"]:
+        print("Gender is not correct")#make sure that the input values are within the specified ranges
 else: #give out the creatine clearance (CrCl) using the Cockcroft-Gault formula
     if gender == "male":
         crcl = ((140 - age) * weight) / (72 * cr)
