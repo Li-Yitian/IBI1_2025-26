58ead296a7794bc025d8b927b77f9ffa505c77e0
liyitian <yitian.25@intl.zju.edu.cn>
2026-03-13 10:32:17 +0800

    add new pseudocode

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -1,7 +1,15 @@
-#age<100 80>weight>20 
-# creatine concentration>0 and <100
+#input the age, weight, creatine concentration, and gender
+#Check if the input values are within the specified ranges:
+#age<100 20<weight<80 
+#creatine concentration>0 and <100
 #gender male/female
-#If one of these conditions is not met, do not report CrCl but instead which input variable needs corrected.
+#if all the conditions are met, calculate the creatinine clearance (CrCl) using the Cockcroft-Gault equation.
+    #if one of these conditions is not met, do not report CrCl but instead which input variable needs corrected.
+    #if age >= 100, report "Age is not correct" end if
+    #if weight < 20 or weight > 80, report "Weight is not correct" end if
+    #if creatine concentration < 0 or creatine concentration > 100, report "Creatine concentration is not correct" end if
+    #if gender is not male or female, report "Gender is not correct" end if
+#else: all conditions are met, report the creatine clearance (CrCl) using the Cockcroft-Gault formula: CrCl according to the gender
 age = float(input("Please enter the age: "))
 weight = float(input("Please enter the weight: "))
 cr = float(input("Please enter the creatine concentration: "))
