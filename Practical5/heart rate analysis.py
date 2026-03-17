0e811f3e15b12d0664245f3889ebda2780d081fe
liyitian <yitian.25@intl.zju.edu.cn>
2026-03-17 18:35:40 +0800

    finish q1 and half of q2

---
+++ Practical5/heart rate analysis.py
@@ -0,0 +1,23 @@
+heart_rates = [72,60,126,85,90,59,76,131,88,121,64]
+n=len(heart_rates)
+mean=sum(heart_rates)/n
+print("Patient number:", n)
+print("Mean heart rate:", round(mean, 2))
+#categorize heart rates low (<60), normal (60-120), high (>120)
+low = 0
+normal = 0
+high = 0
+for rate in heart_rates:
+    if rate < 60:
+        low += 1
+    elif 60 <= rate <= 120:
+        normal += 1 
+    else:        
+        high += 1
+print("Low heart rates number:", low)
+print("Normal heart rates number:", normal)
+print("High heart rates number:", high)
+categories = {"Low": low, "Normal": normal, "High": high}
+max_category = max(categories, key=categories.get)
+print("Most common heart rate category:", max_category)
+
